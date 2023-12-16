cc1e31c4864801e4abd23502653abf8b2e9f6556
CrisMoreiras <137789637+CrisMoreiras@users.noreply.github.com>
2023-12-16 18:15:12 +0000

    01

--- src/models.py
+++ src/models.py
@@ -25,6 +25,36 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+
+class User(Base):
+    __tablename__ = 'usuario'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    last_name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    user_name = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+
+class Character(Base):
+    __tablename__ = 'character'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    gender = Column(String(250), nullable=False)
+    species = Column(String(250), nullable=False)
+    planet = Column(String(250), nullable=False)
+    
+class Planet(Base):
+    __tablename__ = 'planet'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    region = Column(String(250), nullable=False)
+    coordinates = Column(String(250), nullable=False)
+    climate = Column(String(250), nullable=False)
+    
+
+
     def to_dict(self):
         return {}
 
